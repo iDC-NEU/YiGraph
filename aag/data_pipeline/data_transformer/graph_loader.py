9b906f7068cf95602048e53a937022bde50dd31c
Hao Yuan <arrangeman@163.com>
2025-11-16 21:45:58 +0800

    feat: support query by name (#10)
    * load data from raw files
    
    * set rank to index
    
    * feat: support query by name

--- aag/data_pipeline/data_transformer/graph_loader.py
+++ aag/data_pipeline/data_transformer/graph_loader.py
@@ -3,6 +3,7 @@ from typing import Dict, Optional
 from aag.config.data_upload_config import *
 from aag.expert_search_engine.database.datatype import VertexData, EdgeData
 from aag.expert_search_engine.database.nebulagraph import  NebulaGraphClient
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 
@@ -84,7 +85,123 @@ class GraphDataLoader:
             print(f"[INFO] Loaded {len(self.dataset_schemas)} datasets from {self.schema_path}")
         except Exception as e:
             raise RuntimeError(f"[ERROR] Failed to load dataset schemas from {self.schema_path}: {e}")
+    
 
+    def get_field_data(self, config, key):
+        from dataclasses import fields
+
+        result = []
+
+        for f in fields(config):
+            if key in f.name:
+                value = getattr(config, f.name)
+
+                if value is None:
+                    continue  # 跳过 None
+
+                if isinstance(value, list):
+                    result += value
+                else:
+                    result.append(value)
+
+
+        return list(set(result))
+
+
+    def load_data_from_raw(self, data_config):
+
+        all_dfs = []
+
+        for config in data_config:
+
+            all_fields = self.get_field_data(config, "field")
+
+            logger.info(f"Reading ({all_fields}) fields from raw file")
+
+            df = pd.read_csv(config.path, usecols=all_fields)
+
+            # only for vetext data
+            query_field = self.get_field_name(config, "query_field")
+            if query_field and query_field not in df.columns:
+                # print(f"{config.id_field=}")
+                df[query_field] = df[config.query_field].astype(str).agg(" ".join, axis=1)
+
+            all_dfs.append(df)
+
+        merged_df = pd.concat(all_dfs, ignore_index=True)
+        # print(merged_df.head())
+        # print(f"{len(merged_df)=}")
+
+        return merged_df
+
+
+    def get_field_name(self, config, field):
+        item = getattr(config, field, None)
+
+        if item is None:
+            return None
+
+        if isinstance(item, list):
+            return "_".join(item)
+
+        if isinstance(item, str):
+            return item
+
+    
+    def get_graph_content_from_raw(self, dataset_config: DatasetConfig):
+
+        vertex_config = dataset_config.schema.vertex
+        edge_config = dataset_config.schema.edge
+
+        vertex_df = self.load_data_from_raw(vertex_config)
+        edge_df = self.load_data_from_raw(edge_config)
+
+        # process vertex: vid = query_name
+        vertices = []
+        query_name = self.get_field_name(vertex_config[0], "query_field")
+        id_field = vertex_config[0].id_field
+        for _, row in vertex_df.iterrows():
+            vid = str(row[query_name])
+            props = row.drop(query_name).to_dict()          
+            vertices.append(VertexData(vid=vid, properties=props))
+        
+        # print(f"{len(vertices)=}")
+        # print(vertices[0:5])
+
+
+        # process edge: src => query_name, dst => query_name
+        edges = []
+        src_field = edge_config[0].source_field
+        dst_field = edge_config[0].target_field
+        id_to_query = dict(zip(vertex_df[id_field], vertex_df[query_name]))
+        for _, row in edge_df.iterrows():
+            src_raw = row[src_field]
+            dst_raw = row[dst_field]
+
+            try:
+                src = str(id_to_query[src_raw])
+                dst = str(id_to_query[dst_raw])
+            except KeyError:
+                raise ValueError(
+                    f"Edge references unknown id: src={src_raw}, dst={dst_raw}"
+                )
+
+            # modify rank when need
+            rank = _
+            rank_field = None
+
+            props = row.drop([src_field, dst_field] + ([rank_field] if rank_field else [])).to_dict()
+
+            edges.append(EdgeData(src=src, dst=dst, rank=rank, properties=props))
+        
+        
+        # print(f"{len(edges)=}")
+        # print(edges[0])
+
+        return vertices, edges
+    
+    
+    
     def get_graph_content(self, dataset_config: DatasetConfig):
         """
         从图数据库中加载指定数据集的完整图结构。
