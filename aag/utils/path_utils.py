1e46fd24d790167da4c0dae34edbf1c750aacb61
superccy <1013858730@qq.com>
2025-11-26 16:19:01 +0800

    update path_utils

--- aag/utils/path_utils.py
+++ aag/utils/path_utils.py
@@ -16,9 +16,13 @@ KNOWLEDGE_PATH = KNOWLEDGE_BASE_DIR / "knowledge.yaml"
 DEFAULT_CONFIG_PATH = PROJECT_ROOT.parent / "config" / "engine_config.yaml"
 DEFAULT_DATA_UPLOAD_CONFIG_PATH = PROJECT_ROOT.parent / "config" / "data_upload_config.yaml"
 
-DATA_SCHEMA_PATH = PROJECT_ROOT / "data_pipeline" / "data_transformer" / "dataset_schemas"
-GRAPH_SCHEMA_PATH = DATA_SCHEMA_PATH / "graph_schemas.yaml"
-TABLE_SCHEMA_PATH = DATA_SCHEMA_PATH / "table_schemas.yaml"
-TEXT_SCHEMA_PATH = DATA_SCHEMA_PATH / "text_schemas.yaml"
+DATA_SCHEMA_PATH2 = PROJECT_ROOT / "data_pipeline" / "data_transformer" / "dataset_schemas"
+GRAPH_SCHEMA_PATH = DATA_SCHEMA_PATH2 / "graph_schemas.yaml"
+TABLE_SCHEMA_PATH = DATA_SCHEMA_PATH2 / "table_schemas.yaml"
+TEXT_SCHEMA_PATH = DATA_SCHEMA_PATH2 / "text_schemas.yaml"
 
 DEFAULT_CONFIG_SERVER_PATH =  PROJECT_ROOT / "computing_engine" / "config_servers.yaml"
+
+DATA_SCHEMA_PATH = PROJECT_ROOT / "datasets" / "dataset_schemas"
+DATASETS_INDEX_PATH = DATA_SCHEMA_PATH / "datasets.yaml"
+DATA_PATH = PROJECT_ROOT / "datasets" / "data"
